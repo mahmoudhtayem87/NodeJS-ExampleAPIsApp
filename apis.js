9471e578c3b9e61b1dcc05a93e7eebba6ac3a24b
mahmoudtayem <mahmoudhtayem@outlook.com>
2022-03-04 21:23:37 +0400

    commit on master

--- apis.js
+++ apis.js
@@ -1,29 +1,20 @@
-
-
 const mysql = require('mysql2/promise');
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 const port = 3000;
-
 // Where we will keep books
 let applications = [];
-
 app.use(cors());
-
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.post('/application', (req, res) => {
     const application = req.body;
-
     // Output the book to the console for debugging
     console.log(application);
     applications.push(application);
-
     res.send('application is added to the database');
 });
 app.get('/applications', (req, res) => {
